f0c4da1d0972ccb6a9b4b77eda1188476fe29cc0
johnnygato <joao88vsc@gmail.com>
2021-11-05 19:41:42 +0000

    setup project

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Instagram Clone</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+    </div>
+  );
+}
